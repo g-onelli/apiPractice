c434cd904d76ec9136c68109a538257c0384a387
g-onelli <63609012+g-onelli@users.noreply.github.com>
2021-10-09 12:16:04 -0400

    Add files via upload

---
+++ hw4_test_api_program_onelli.py
@@ -0,0 +1,32 @@
+"""
+Gabriela Onelli
+Dr. Saremi
+SSW 567
+10/02/2021
+
+Homework 4: Developing with Testers in Mind
+I pledge my honor that I have abided by the Stevens Honor System. -Gabriela Onelli
+"""
+import unittest
+import hw4_api_program_onelli
+
+class TestAPIProgram(unittest.TestCase):
+    def test_accessAccount(self):
+        account = 'richkempinski'
+        result = hw4_api_program_onelli.get_github_repos(account)
+        self.assertEqual(type(result),'dict')
+        self.assertNotEqual(len(result),0)
+
+    def test_storeRepos(self):
+        repos = {}
+        result = hw4_api_program_onelli.store_repos(repos)
+        self.assertNotEqual(len(result),0)
+
+    def test_RepoData(self):
+        account = 'richkempinski'
+        repos = {}
+        result = hw4_api_program_onelli.get_github_repo_data(repos,account)
+        self.assertNotEqual(len(result),0)
+
+if __name__ == '__main__':
+    unittest.main()
